c89f24ba946f87fd0cecd2c9ad07b71954af8fa8
Humberto E. De Leon <humberto0118@gmail.com>
2019-11-29 00:00:45 -0500

    removed google analytics code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/build'));
+app.use('/', express.static(__dirname + '/build/'));
 
 app.listen(PORT, () => {
 	console.log(`Listening with success at port ${PORT}.`);
--- src/config.js
+++ src/config.js
@@ -1,126 +1,122 @@
-import React from "react";
-
 const config = {
-  ROMS: {
-    "1943": {
-      name: "1943",
-      url: "../roms/1943.nes"
-    },
-    "Bugs Bunny Crazy Castle": {
-      name: "Bugs Bunny Crazy Castle",
-      url: "../roms/Bugs Bunny Crazy Castle.nes"
-    },
-    "Castlevania 2 - Simon's Quest (U)": {
-      name: "Castlevania 2 - Simon's Quest (U)",
-      url: "../roms/Castlevania 2 - Simon's Quest (U).nes"
-    },
-    "Castlevania 3 - Dracula's Curse (U)": {
-      name: "Castlevania 3 - Dracula's Curse (U)",
-      url: "../roms/Castlevania 3 - Dracula's Curse (U).nes"
-    },
-    Castlevania: {
-      name: "Castlevania",
-      url: "../roms/Castlevania.nes"
-    },
-    "Guerrilla War (U)": {
-      name: "Guerrilla War (U)",
-      url: "../roms/Guerrilla War (U).nes"
-    },
-    "Star Trek - 25th Anniversary": {
-      name: "Star Trek - 25th Anniversary",
-      url: "../roms/Star Trek - 25th Anniversary.nes"
-    },
-    "Star Trek - The Next Generation (U)": {
-      name: "Star Trek - The Next Generation (U)",
-      url: "../roms/Star Trek - The Next Generation (U).nes"
-    },
-    "Star Voyager": {
-      name: "Star Voyager",
-      url: "../roms/Star Voyager.nes"
-    },
-    "Star Wars - The Empire Strikes Back": {
-      name: "Star Wars - The Empire Strikes Back",
-      url: "../roms/Star Wars - The Empire Strikes Back.nes"
-    },
-    "Star Wars": {
-      name: "Star Wars",
-      url: "../roms/Star Wars.nes"
-    },
-    "Starship Hector": {
-      name: "Starship Hector",
-      url: "../roms/Starship Hector.nes"
-    },
-    "Super Mario Bros 2": {
-      name: "Super Mario Bros 2",
-      url: "../roms/Super Mario Bros 2.nes"
-    },
-    "Super Mario Bros 3": {
-      name: "Super Mario Bros 3",
-      url: "../roms/Super Mario Bros 3.nes"
-    },
-    Yoshi: {
-      name: "Yoshi",
-      url: "../roms/Yoshi.nes"
-    },
-    "Zelda 2 - The Adventure of Link": {
-      name: "Zelda 2 - The Adventure of Link",
-      url: "../roms/Zelda 2 - The Adventure of Link.nes"
-    },
-    arkanoid: {
-      name: "arkanoid",
-      url: "../roms/arkanoid.nes"
-    },
-    "contra-1": {
-      name: "contra-1",
-      url: "../roms/contra-1.nes"
-    },
-    croom: {
-      name: "croom",
-      url: "../roms/croom.nes"
-    },
-    galaga: {
-      name: "galaga",
-      url: "../roms/galaga.nes"
-    },
-    "kirby-adventures-1": {
-      name: "kirby-adventures-1",
-      url: "../roms/kirby-adventures-1.nes"
-    },
-    lj65: {
-      name: "lj65",
-      url: "../roms/lj65.nes"
-    },
-    "lunar-pool": {
-      name: "lunar-pool",
-      url: "../roms/lunar-pool.nes"
-    },
-    mappy: {
-      name: "mappy",
-      url: "../roms/mappy.nes"
-    },
-    "pac-man": {
-      name: "pac-man",
-      url: "../roms/pac-man.nes"
-    },
-    "road-fighter": {
-      name: "road-fighter",
-      url: "../roms/road-fighter.nes"
-    },
-    starforce: {
-      name: "starforce",
-      url: "../roms/starforce.nes"
-    },
-    "tetris-original": {
-      name: "tetris-original",
-      url: "../roms/tetris-original.nes"
-    },
-    xevious: {
-      name: "xevious",
-      url: "../roms/xevious.nes"
-    }
-  },
-  GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
-  SENTRY_URI: process.env.REACT_APP_SENTRY_URI
+	ROMS: {
+		'1943': {
+			name: '1943',
+			url: '../roms/1943.nes'
+		},
+		'Bugs Bunny Crazy Castle': {
+			name: 'Bugs Bunny Crazy Castle',
+			url: '../roms/Bugs Bunny Crazy Castle.nes'
+		},
+		"Castlevania 2 - Simon's Quest (U)": {
+			name: "Castlevania 2 - Simon's Quest (U)",
+			url: "../roms/Castlevania 2 - Simon's Quest (U).nes"
+		},
+		"Castlevania 3 - Dracula's Curse (U)": {
+			name: "Castlevania 3 - Dracula's Curse (U)",
+			url: "../roms/Castlevania 3 - Dracula's Curse (U).nes"
+		},
+		Castlevania: {
+			name: 'Castlevania',
+			url: '../roms/Castlevania.nes'
+		},
+		'Guerrilla War (U)': {
+			name: 'Guerrilla War (U)',
+			url: '../roms/Guerrilla War (U).nes'
+		},
+		'Star Trek - 25th Anniversary': {
+			name: 'Star Trek - 25th Anniversary',
+			url: '../roms/Star Trek - 25th Anniversary.nes'
+		},
+		'Star Trek - The Next Generation (U)': {
+			name: 'Star Trek - The Next Generation (U)',
+			url: '../roms/Star Trek - The Next Generation (U).nes'
+		},
+		'Star Voyager': {
+			name: 'Star Voyager',
+			url: '../roms/Star Voyager.nes'
+		},
+		'Star Wars - The Empire Strikes Back': {
+			name: 'Star Wars - The Empire Strikes Back',
+			url: '../roms/Star Wars - The Empire Strikes Back.nes'
+		},
+		'Star Wars': {
+			name: 'Star Wars',
+			url: '../roms/Star Wars.nes'
+		},
+		'Starship Hector': {
+			name: 'Starship Hector',
+			url: '../roms/Starship Hector.nes'
+		},
+		'Super Mario Bros 2': {
+			name: 'Super Mario Bros 2',
+			url: '../roms/Super Mario Bros 2.nes'
+		},
+		'Super Mario Bros 3': {
+			name: 'Super Mario Bros 3',
+			url: '../roms/Super Mario Bros 3.nes'
+		},
+		Yoshi: {
+			name: 'Yoshi',
+			url: '../roms/Yoshi.nes'
+		},
+		'Zelda 2 - The Adventure of Link': {
+			name: 'Zelda 2 - The Adventure of Link',
+			url: '../roms/Zelda 2 - The Adventure of Link.nes'
+		},
+		arkanoid: {
+			name: 'arkanoid',
+			url: '../roms/arkanoid.nes'
+		},
+		'contra-1': {
+			name: 'contra-1',
+			url: '../roms/contra-1.nes'
+		},
+		croom: {
+			name: 'croom',
+			url: '../roms/croom.nes'
+		},
+		galaga: {
+			name: 'galaga',
+			url: '../roms/galaga.nes'
+		},
+		'kirby-adventures-1': {
+			name: 'kirby-adventures-1',
+			url: '../roms/kirby-adventures-1.nes'
+		},
+		lj65: {
+			name: 'lj65',
+			url: '../roms/lj65.nes'
+		},
+		'lunar-pool': {
+			name: 'lunar-pool',
+			url: '../roms/lunar-pool.nes'
+		},
+		mappy: {
+			name: 'mappy',
+			url: '../roms/mappy.nes'
+		},
+		'pac-man': {
+			name: 'pac-man',
+			url: '../roms/pac-man.nes'
+		},
+		'road-fighter': {
+			name: 'road-fighter',
+			url: '../roms/road-fighter.nes'
+		},
+		starforce: {
+			name: 'starforce',
+			url: '../roms/starforce.nes'
+		},
+		'tetris-original': {
+			name: 'tetris-original',
+			url: '../roms/tetris-original.nes'
+		},
+		xevious: {
+			name: 'xevious',
+			url: '../roms/xevious.nes'
+		}
+	}
 };
 
 export default config;
