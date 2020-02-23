cc29db6f3c71c0489251b4645e5900437618d7c8
Enrique Shunnar <xtealer00@gmail.com>
2020-02-23 17:29:15 -0500

    updated root serve path

--- src/App.js
+++ src/App.js
@@ -6,38 +6,38 @@ import { handleError } from './utils';
 import './App.css';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { error: null };
-	}
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
 
-	render() {
-		if (this.state.error) {
-			return (
-				<div className="container my-4">
-					<div className="row justify-content-center">
-						<div className="col-md-8">
-							Oops - there has been an error. It has been logged to the console.
-						</div>
-					</div>
-				</div>
-			);
-		}
-		return (
-			<BrowserRouter>
-				<div className="App">
-					<Route exact path="/" component={ListPage} />
-					<Route exact path="/run" component={RunPage} />
-					<Route exact path="/run/:slug" component={RunPage} />
-				</div>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    if (this.state.error) {
+      return (
+        <div className="container my-4">
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              Oops - there has been an error. It has been logged to the console.
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <BrowserRouter basename={'/nes'}>
+        <div className="App">
+          <Route exact path="/" component={ListPage} />
+          <Route exact path="/run" component={RunPage} />
+          <Route exact path="/run/:slug" component={RunPage} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 
-	componentDidCatch(error, errorInfo) {
-		this.setState({ error });
-		handleError(error, errorInfo);
-	}
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+    handleError(error, errorInfo);
+  }
 }
 
 export default App;
